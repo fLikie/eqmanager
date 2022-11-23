193e6e2c22ecf37acd3165928e0a1673f50f2d6a
flikie <fLiki3@gmail.com>
2022-11-23 21:02:16 +0600

    add Procfile for heroku add test login and signup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT -jar build/libs/eqmanager-0.0.1-SNAPSHOT.jar
--- src/main/kotlin/com/example/eqmanager/LoginController.kt
+++ src/main/kotlin/com/example/eqmanager/LoginController.kt
@@ -1,18 +1,21 @@
 package com.example.eqmanager
 
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
 
 @Controller
 class LoginController {
 
     @PostMapping("/login")
-    fun login() {
-
+    fun login(): ResponseEntity<String> {
+        return ResponseEntity.ok("login")
     }
 
     @PostMapping("/signup")
-    fun signUp() {
-
+    fun signUp(): Result<String> {
+        return Result.success("signup")
     }
 }
