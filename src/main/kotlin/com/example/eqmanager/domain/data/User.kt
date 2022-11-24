195adad0fb6d1449756f765f07bbb532541b6d1a
flikie <fLiki3@gmail.com>
2022-11-25 00:57:36 +0600

    change tbl

--- src/main/kotlin/com/example/eqmanager/domain/data/User.kt
+++ src/main/kotlin/com/example/eqmanager/domain/data/User.kt
@@ -3,7 +3,7 @@ package com.example.eqmanager.domain.data
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 
-@Table("EQMANAGER.USER_TBL")
+@Table("USER_TBL")
 data class User(
     val phone: String,
 )
