d7888f921fcf858a45001d9cc8f28efdd0ccba3c
flikie <fLiki3@gmail.com>
2022-11-25 01:01:42 +0600

    schema add

--- src/main/kotlin/com/example/eqmanager/domain/data/User.kt
+++ src/main/kotlin/com/example/eqmanager/domain/data/User.kt
@@ -2,8 +2,9 @@ package com.example.eqmanager.domain.data
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
+import javax.xml.validation.Schema
 
-@Table("USER_TBL")
+@Table(schema = "EQMANAGER", name = "USER_TBL")
 data class User(
     val phone: String,
 )
