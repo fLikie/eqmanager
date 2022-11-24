70b10593a49178d566c7e6eb9427dd60ee05ed08
flikie <fLiki3@gmail.com>
2022-11-24 17:57:42 +0600

    add database

---
+++ src/main/kotlin/com/example/eqmanager/MarkerController.kt
@@ -0,0 +1,13 @@
+package com.example.eqmanager
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class MarkerController {
+
+    @GetMapping("/getmarkers")
+    fun getMarkers() {
+
+    }
+}
