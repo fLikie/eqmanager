42cf0bc2484892c60aa3a56d6e65654f63706dd7
flikie <fLiki3@gmail.com>
2022-11-26 02:26:21 +0600

    mapping fix

--- src/main/kotlin/com/example/eqmanager/domain/data/user/User.kt
+++ src/main/kotlin/com/example/eqmanager/domain/data/user/User.kt
@@ -1,8 +1,10 @@
 package com.example.eqmanager.domain.data.user
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.data.relational.core.mapping.Table
 
 @Table(schema = "EQMANAGER", name = "USER_TBL")
 data class User(
+    @JsonProperty("phone")
     val phone: String,
 )
