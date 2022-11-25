c407a8cb68a3a4713ee2ece84a7953b2b18f083d
flikie <fLiki3@gmail.com>
2022-11-26 05:55:23 +0600

    fix markers api

--- src/main/kotlin/com/example/eqmanager/domain/data/markers/Marker.kt
+++ src/main/kotlin/com/example/eqmanager/domain/data/markers/Marker.kt
@@ -1,10 +1,18 @@
 package com.example.eqmanager.domain.data.markers
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class Marker(
+    @JsonProperty("x_Coordinate")
     val X_Coordinate: String? = "",
+    @JsonProperty("y_Coordinate")
     val Y_Coordinate: String? = "",
+    @JsonProperty("comments")
     val comments: String? = "",
+    @JsonProperty("plusCount")
     val plusCount: Int? = 0,
+    @JsonProperty("minusCount")
     val minusCount: Int? = 0,
+    @JsonProperty("approved")
     val approved: Boolean? = false
 )
