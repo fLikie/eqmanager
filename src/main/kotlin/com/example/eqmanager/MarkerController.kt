c5899c36f4bae7a76bb4b54e028c5d14ba3e07de
flikie <fLiki3@gmail.com>
2022-11-26 06:04:22 +0600

    fix markers api

--- src/main/kotlin/com/example/eqmanager/MarkerController.kt
+++ src/main/kotlin/com/example/eqmanager/MarkerController.kt
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 
 @Controller
 class MarkerController(
@@ -23,7 +24,7 @@ class MarkerController(
     }
 
     @PostMapping("/addmarker")
-    fun addMarker(marker: Marker): ResponseEntity<Response> {
+    fun addMarker(@RequestBody marker: Marker): ResponseEntity<Response> {
         return ResponseEntity.ok(Response(markerService.saveMarker(marker)))
     }
 }
